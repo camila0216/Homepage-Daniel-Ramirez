51fbe36e8df4c32b314bb2af8c3aec6eaa8e5ddf
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-08-18 12:22:16 -0500

    first_change

---
+++ js/index.js
@@ -0,0 +1,77 @@
+function moveImage() {
+  let element = document.getElementById("photo");
+  let respPhone = window.matchMedia("(max-width: 575.98px)");
+  let pos = -50;
+  let id = setInterval(frame, 10);
+  function frame() {
+    if (respPhone.matches) {
+
+    }
+    else{
+      if (pos == 24) {
+        clearInterval(id);
+      } else {
+        pos++;
+        element.style.marginLeft = pos + "vw";
+      }
+    }
+  }
+}
+
+function putImageStudies(){
+  let element = document.getElementById("imagenDos");
+  let respPhone = window.matchMedia("(max-width: 575.98px)");
+  element.src = "images/logo.png";
+  if (respPhone.matches) {
+    element.style.width = 30 + "vw";
+    element.style.height = 10 + "vh";
+    element.style.marginLeft = -2 + "vw";
+  }
+  else
+  {
+    element.style.marginLeft = -3 + "vw";
+  }
+}
+
+function putImageProjects(){
+  let element = document.getElementById("imagenDos");
+  let respPhone = window.matchMedia("(max-width: 575.98px)");
+  element.src = "images/nami.png";
+  element.style.backgroundColor = "rgb(41, 41, 41);";
+  if (respPhone.matches) {
+    element.style.width = 30 + "vw";
+    element.style.height = 10 + "vh";
+    element.style.marginLeft = -2 + "vw";
+  }
+  else
+  {
+    element.style.marginLeft = -3 + "vw";
+  }
+}
+
+function putImageHobbies(){
+  let element = document.getElementById("imagenDos");
+  let respPhone = window.matchMedia("(max-width: 575.98px)");
+  element.src = "images/control.png";
+  element.style.backgroundColor = "rgb(41, 41, 41);";
+  if (respPhone.matches) {
+    element.style.width = 30 + "vw";
+    element.style.height = 10 + "vh";
+    element.style.marginLeft = -2 + "vw";
+  }
+  else
+  {
+    element.style.marginLeft = -3 + "vw";
+  }
+}
+
+/* function backImageTwo(){
+  let element = document.getElementById("imagenDos");
+  element.src = undefined;
+  element.alt = "Welcome you all";
+  element.style.backgroundColor = "rgb(41, 41, 41);";
+} */
+
+/* function rotateContact(){
+
+} */
